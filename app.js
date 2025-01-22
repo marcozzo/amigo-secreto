8ad08e3ba7edeb2a029016154a203f182bb779b8
marcozzo <130609450+marcozzo@users.noreply.github.com>
2025-01-21 23:35:04 -0300

    Adicionar checagem de campo vazio

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 let amigos = [];
+
+function adicionarAmigo() {
+    let nome = document.getElementById('amigo');
+    if(nome.value == ""){
+        alert('Insira um nome!')
+        return
+    }
+}
